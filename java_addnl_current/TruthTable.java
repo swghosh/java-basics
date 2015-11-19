3babde612df8369494d39873ee3c29a9bfb932eb
SwG Ghosh <snwg@live.com>
2015-11-19 22:07:11 +0530

    Added Truth

---
+++ java_addnl_current/TruthTable.java
@@ -0,0 +1,15 @@
+public class TruthTable {
+    private static void printTruthTable(int n) {
+        int rows = (int) Math.pow(2,n);
+
+        for (int i=0; i<rows; i++) {
+            for (int j=n-1; j>=0; j--) {
+                System.out.print((i/(int) Math.pow(2, j))%2 + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        printTruthTable(3); //enter any natural int
+    }
+}
