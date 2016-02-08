2cb31cb271702d2680c6b22b3ddc200c485ead53
SwG Ghosh <codecrafts.cf@icloud.com>
2016-02-08 10:19:13 +0530

    All Java Files Organized into Folders. #KeepItSimple

---
+++ SmallProject3/Array.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class Array
+{
+    static Arrayer a;
+    /**
+     * Constructor for objects of class Array
+     */
+    public Array()
+    {
+    }
+
+    /**
+     * Menu Drien Interface
+     */
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("---MENU---");
+        System.out.println("1 - Insert elements into array");
+        System.out.println("2 - Delete elements from array");
+        System.out.println("3 - Print elements of array");
+        System.out.println("0 - Exit");
+        System.out.print("\nEnter your choice : ");
+        int ch=s.nextInt();
+        switch(ch)
+        {
+            case 1:
+            System.out.print("\nEnter size of array : ");
+            int size=s.nextInt();
+            a=new Arrayer(size);
+            System.out.print("\nEnter array's elements.");
+            a.insertElements();
+            break;
+            case 2:
+            System.out.print("\nEnter position of array's element to delete : ");
+            int pos=s.nextInt()-1;
+            a.deleteElement(pos);
+            break;
+            case 3:
+            System.out.print("\nElements of the array.");
+            a.printElements();
+            break;
+            case 0:
+            System.exit(0);
+            break;
+            default:
+            System.out.print("\nInvalid Choice! Please Retry.\n\n");
+        }
+    }
+}
--- cpppkg/general.cpp
+++
@@ -1,7 +0,0 @@
-#include<stdlib.h>
-#include<iostream>
-using namespace std;
-int main()
-{
-	system("cls");
-}
