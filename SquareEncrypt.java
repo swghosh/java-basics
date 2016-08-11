96162ccb10860fc662aa6ea6ba6f669d351a5827
SwG Ghosh <codecrafts.cf@icloud.com>
2016-08-11 12:28:12 +0530

    Added SquareEncryptor

---
+++ SquareEncrypt.java
@@ -0,0 +1,30 @@
+class SquareEncrypt {
+
+    static String encrypt(String str) {
+        //Replace all instances of space with asteriks
+        str = str.replaceAll(" ", "*");
+        //Find the number of rows required
+        int n = ((str.length() % 6 == 0) ? (str.length()/6) : ((str.length()/6) + 1));
+        //Initialise a character array for encryption
+        char c[][] = new char[n][6];
+        //Insert charcaters into the array
+        for(int i = 0, k = 0; i < c.length; i++) {
+            for(int j = 0; j < c[i].length; j++) {
+                if(k < str.length()) c[i][j] = str.charAt(k++);
+                else c[i][j] = '.';
+            }
+        }
+        //Initialise the string that will store the encrypted string
+        String code = "";
+        //Put the array elements into string based on encryption pattern
+        for(int i = 0; i < c[0].length; i++) {
+            for(int j = 0; j < c.length; j++) {
+                code = code + c[j][i];
+            }
+        }
+        //Encrypted string returned
+        return code;
+    }
+    
+    
+}
